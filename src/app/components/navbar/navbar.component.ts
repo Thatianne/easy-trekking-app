aec6c2e89bbca90c7d07737937f74aa0275fc388
Thatianne Carvalho <thatianne@vizir.com.br>
2023-05-23 20:37:41 -0300

    Add admin menu options

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '@models/user';
 import { UserService } from '@services/user/user.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { RoleEnum } from 'src/app/enums/role.enum';
+
+interface MenuOption {
+  label: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  user$!: Observable<User | null>;
+  menuOptions$ = new BehaviorSubject<MenuOption[]>([]);
 
   constructor(private _userService: UserService, private _router: Router){}
+
+  ngOnInit(): void {
+    this.user$ = this._userService.getUser();
+
+    this.user$.subscribe(user => {
+      if (user?.role.id) {
+        if (user.role.id === RoleEnum.Administrator) {
+          this.menuOptions$.next([{
+            label: 'Trekkings',
+            link: 'admin/trekkings'
+          },
+          {
+            label: 'Tarefas',
+            link: 'admin/automated-tasks'
+          },
+          {
+            label: 'Grupos',
+            link: 'admin/groups'
+          }])
+        } else {
+          this.menuOptions$.next([])
+        }
+      }
+    })
+  }
 
   onLogout(): void {
     this._userService.logout();
